6edf34771984d4f77bac545faa651ad0e5ef69ba
sumithgs <sumithgs2000@gmail.com>
2021-04-12 11:26:47 +0530

    made changes to push to heroku

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -19,10 +19,17 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from mywebapp import views
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mywebapp.urls')),
     path('user_login/', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='logout'),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
