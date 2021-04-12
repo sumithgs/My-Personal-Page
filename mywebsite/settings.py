f693f3a3f6a04e4107f421af7e3c8c31b639bbc0
sumithgs <sumithgs2000@gmail.com>
2021-04-12 22:48:42 +0530

    adding settings changes

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -162,6 +162,7 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_USE_SSL = False
 
 
